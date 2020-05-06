27bcbc0ab4c224edd491a7126ab7c98366518af6
darshan-ar <60151901+darshan-ar@users.noreply.github.com>
2020-05-06 00:23:41 -0400

    Comparison codes

---
+++ Benchmark.py
@@ -0,0 +1,6 @@
+import rsep,lowest_mcs,average_mcs,random_mcs,highest_mcs
+
+M = print("Enter the number of MVNO's: ")
+B = print("Enter the number of Base Stations: ")
+print("Now Benchmarking our algorithm with static allocation methods")
+
